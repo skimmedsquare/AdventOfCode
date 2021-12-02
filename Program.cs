e850cf99d1b19791bd0cb10e91a710a919d6e548
Sean Kim <seanctkim@gmail.com>
2021-12-01 22:47:42 -0800

    Day one complete

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Aoc2021.DayOne;
+
+namespace Aoc2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Make solution solver nicer CLI.
+            Console.WriteLine($"Solution: {DepthSolver.SolvePartTwo()}");
+        }
+    }
+}
---
+++ day.one/PartOneSolver.cs
@@ -0,0 +1,49 @@
+namespace Aoc2021 {
+    namespace DayOne {
+        class DepthSolver {
+            public static int SolvePartOne() {
+                List<int> depths = ReadInput();
+                return NumIncreasing(depths);
+            }    
+
+            public static int SolvePartTwo() {
+                List<int> depths = ReadInput();
+
+                List<int> windows = new List<int>(depths.Count - 3);
+                var currWindow = 0;
+                for (var i = 0; i < depths.Count - 2; i++) {
+                    if (i == 0) { 
+                        currWindow = depths[i] + depths[i+1] + depths[i+2];
+                        windows.Add(currWindow);
+                    } else {
+                        currWindow -= depths[i-1];
+                        currWindow += depths[i+2];
+                        windows.Add(currWindow);
+                    }
+                }
+
+                return NumIncreasing(windows);
+            }
+
+            static List<int> ReadInput() {
+                return System.IO.File.ReadLines("day.one/part-one.txt")
+                    .Select(line => int.Parse(line))
+                    .ToList();
+            }
+
+            static int NumIncreasing(in List<int> vals ) {
+                var count = 0;
+                for (var i = 0; i < vals.Count; i++) {
+                    if (i == 0) {
+                        continue;
+                    }
+                    if (vals[i] > vals[i-1]) {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+    }
+}
